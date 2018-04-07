d3b88442de19e272c892099b7d1bc5ce8fe1655a
Mike <mlh1477@gmail.com>
2018-04-07 13:20:54 -0400

    Style background video

--- template-parts/background-video.php
+++ template-parts/background-video.php
@@ -1,3 +1,7 @@
 <div class="background-video">
 
+  <!-- Get the video out of the post by post id -->
+  <?php $background_video = get_post(50); ?>
+  <?php echo $background_video->post_content; ?>
+
 </div>
